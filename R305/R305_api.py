b675524444bc0b1c3651bb7330083e571d04988d
girish <girish946@gmail.com>
2016-02-10 22:55:00 +0530

    changes for python3

---
+++ R305/R305_api.py
@@ -0,0 +1,104 @@
+import serial
+import time
+#from R305 import generateHeader, header, address
+import genImg, Img2Tz, RegModel, TemplateNum, DeleteAll, Store
+
+ser = serial.Serial('/dev/ttyAMA0')
+
+
+def setBaud():
+    ser.baudrate = 57600
+
+
+def write():
+
+    
+    #first finger scan
+    setBaud()    
+    data = genImg.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    genImg.parse(s)
+    # generate character file of the finger image.
+    setBaud()
+    data = Img2Tz.getHeader(0x01)
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    Img2Tz.parse(s)
+
+
+    #second finger scan.
+    setBaud()    
+    data = genImg.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    genImg.parse(s)
+    #generate character file of the finger image.
+    setBaud()
+    data = Img2Tz.getHeader(0x02)
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    Img2Tz.parse(s)
+
+
+    #create RegModel
+    setBaud()    
+    data = RegModel.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    RegModel.parse(s)
+    #getTemplateNumber
+    setBaud()    
+    data = TemplateNum.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    num = TemplateNum.parse(s)
+    print ("recived template Number is "+ str(num))
+
+    """
+    setBaud()    
+    data = DeleteAll.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    print(DeleteAll.parse(s))"""
+
+    
+    setBaud()    
+    data = Store.getHeader(0x01,int(num)+1 )
+    print([hex(int(c)) for c in data])
+    ser.write(bytearray(int(x) for x in data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    print ( len(s))
+    Store.parse(s)
+
+     #getTemplateNumber
+    setBaud()    
+    data = TemplateNum.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(c) for c in s])
+    num = TemplateNum.parse(s)
+    print( "recived template Number is "+ str(num))    
+
+    
+    
+    
+if __name__ == '__main__':
+    write()
---
+++ R305/TemplateNum.py
@@ -0,0 +1,33 @@
+from struct import unpack
+from R305 import generateHeader, header, address
+
+identifire = {1:"command packet",
+              2: "data packet",
+              7:"acknokedge packet",
+              8:"end of data packte"}
+
+confirmation_codes = {0:"read complete.",
+                      1:"error while reciving package."}
+
+def parse(s):
+    recived_header = s[:2]
+    recived_address = s[2:6]
+    recived_id = s[6]
+    recived_length =s[7:9]
+    recived_c_code= s[9]
+    recived_template_no = s[10:12]
+    if( header == [int(c) for c in recived_header]):
+        if( address == [int(c) for c in recived_address]):
+
+            print ("address and header ok")
+            print(identifire[int(recived_id) ])
+            print (confirmation_codes[int(recived_c_code)])
+            print("template number is: "+str(256*recived_template_no[0] + recived_template_no[1]))
+            if int(recived_c_code) == 0x00:
+                return 256*recived_template_no[0] + recived_template_no[1]
+            else:
+                return "error while reciving data"
+                
+
+def getHeader():
+    return generateHeader()+[0x01, 0x00,0x03,0x1d,0x00,0x21]
